78ad4fc917003f32ba33cfcd7761bfa307121e46
Maui Lontoc <32824583+mauiworketc@users.noreply.github.com>
2023-03-09 14:08:47 +0800

    Update y-prosemirror.js

--- src/y-prosemirror.js
+++ src/y-prosemirror.js
@@ -1,7 +1,7 @@
-export * from './plugins/cursor-plugin.js'
+export { defaultCursorBuilder, defaultSelectionBuilder, createDecorations, yCursorPlugin } from './plugins/cursor-plugin.js'
 export { ySyncPlugin, isVisible, getRelativeSelection, ProsemirrorBinding } from './plugins/sync-plugin.js'
-export * from './plugins/undo-plugin.js'
-export * from './plugins/keys.js'
+export { undo, redo, defaultProtectedNodes, defaultDeleteFilter, yUndoPlugin  } from './plugins/undo-plugin.js'
+export { ySyncPluginKey, yUndoPluginKey, yCursorPluginKey } from './plugins/keys.js'
 export {
   absolutePositionToRelativePosition, relativePositionToAbsolutePosition, setMeta, prosemirrorJSONToYDoc, yDocToProsemirrorJSON, yDocToProsemirror, prosemirrorToYDoc,
   prosemirrorJSONToYXmlFragment, yXmlFragmentToProsemirrorJSON, yXmlFragmentToProsemirror, prosemirrorToYXmlFragment
